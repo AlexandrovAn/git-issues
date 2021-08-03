163100754ca9d7e8281b56cdcd7367699542133f
AlexandrovAn <57633337+AlexandrovAn@users.noreply.github.com>
2021-08-03 16:04:13 +0400

    Update NetworkModule with NetworkProvider functions

--- app/src/main/java/com/petproject/gitissues/di/modules/NetworkModule.kt
+++ app/src/main/java/com/petproject/gitissues/di/modules/NetworkModule.kt
@@ -1,17 +1,24 @@
 package com.petproject.gitissues.di.modules
 
 import com.petproject.gitissues.remote.IssueService
-import com.petproject.gitissues.remote.NetworkProvider
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Singleton
 
 @Module
 class NetworkModule {
 
+    private val retrofitBuilder: Retrofit.Builder by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+    }
+    private val issueService: IssueService by lazy {
+        retrofitBuilder.build().create(IssueService::class.java)
+    }
+
     @Provides
-    fun issueService() = NetworkProvider.issueService
+    fun issueService() = issueService
 
 }
